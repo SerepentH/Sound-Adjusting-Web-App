a84da944185c754625177dfb78ab20f0464c9885
SerepentH <79256227+SerepentH@users.noreply.github.com>
2022-04-25 12:39:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,7 +10,8 @@ function preload()
 	song = loadSound("music.mp3");
 }
 
-function setup() {
+function setup() 
+{
 	canvas =  createCanvas(600, 500);
 	canvas.position(350, 180)
 
@@ -21,7 +22,8 @@ function setup() {
 	poseNet.on('pose', gotPoses);
 }
 
-function modelLoaded() {
+function modelLoaded() 
+{
   console.log('PoseNet Is Initialized');
 }
 
@@ -29,9 +31,9 @@ function gotPoses(results)
 {
   if(results.length > 0)
   {
-	scoreRightWrist =  results[0].pose.keypoints[10].score;
+	console.log(results);
 	scoreLeftWrist =  results[0].pose.keypoints[9].score;
-	console.log("scoreRightWrist = " + scoreRightWrist + " scoreLeftWrist = " + scoreLeftWrist);
+	console.log(" scoreLeftWrist = " + scoreLeftWrist);
 	
 	rightWristX = results[0].pose.rightWrist.x;
 	rightWristY = results[0].pose.rightWrist.y;
@@ -47,6 +49,18 @@ function gotPoses(results)
 function draw()
 {
     image(video, 0, 0, 600, 500);
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreLeftWrist > 0.2)
+	{
+	   circle(leftWristY, leftWristX, 20);
+	   InNumberleftWristY = Number(leftWristY);
+	   remove_decimals = floor(InNumberleftWristY);
+	   volume = remove_decimals/500;
+	   document.getElementById("Volume").innerHTML = "Volume = " + volume;
+	   song.setVolume(volume);
+	}
 }
 
 function play()
